53b6671a7a6df02a3cd9cde1b10eda80ab297645
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-07 17:28:51 -0500

    notify not working with some Polish letters

---
+++ mainnonotify.py
@@ -0,0 +1,28 @@
+# notification doesn't work with certain Polish words
+# /usr/bin/python3 /home2/niemann8/dr2-data/mainnonotify.py 1
+
+import json, subprocess, sys
+import scraper
+
+
+days_ago = int(sys.argv[1]) # 0 would be 'main.py'
+# days_ago = 1
+
+today = scraper.get_challenges(0)
+
+# days_ago gives us a backdoor in case the source site is down one day
+yesterday = scraper.get_challenges(days_ago)
+leaderboards = scraper.get_leaderboards(yesterday)
+
+
+# generate .json files
+# today's challenges
+with open("dr2-data/challenges_{}.json".format(today[0]["start"][0:10]), "w") as f:
+  json.dump(today, f)
+# yesterday's results
+with open("dr2-data/leaderboards_{}.json".format(yesterday[0]["start"][0:10]), "w") as f:
+  json.dump(leaderboards, f)
+
+
+# call the PHP script to insert data into database
+subprocess.call('php /home2/niemann8/dr2-data/insert.php', shell=True)
