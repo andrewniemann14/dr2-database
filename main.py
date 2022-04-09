4d35e336606f59693f5477c9b3d4dc0c420b4ac0
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2022-04-08 23:03:32 -0500

    wrapped everything in try/except

--- main.py
+++ main.py
@@ -3,26 +3,44 @@
 import json, subprocess, sys
 import scraper, notify
 
-
 days_ago = int(sys.argv[1]) # 0 would be 'main.py'
 # days_ago = 1
 
-today = scraper.get_challenges(0)
-notify.notify(today)
+try:
+  today = scraper.get_challenges(0)
+except:
+  print("scraping today didn't work")
+
+try:
+  notify.notify(today)
+except:
+  print("notify didn't work")
 
 # days_ago gives us a backdoor in case the source site is down one day
-yesterday = scraper.get_challenges(days_ago)
-leaderboards = scraper.get_leaderboards(yesterday)
+try:
+  yesterday = scraper.get_challenges(days_ago)
+  leaderboards = scraper.get_leaderboards(yesterday)
+except:
+  print("scraping yesterday didn't work")
 
+try:
+  # today's challenges
+  with open("dr2-data/challenges_{}.json".format(today[0]["start"][0:10]), "w") as f:
+    json.dump(today, f)
+  # yesterday's results
+  with open("dr2-data/leaderboards_{}.json".format(yesterday[0]["start"][0:10]), "w") as f:
+    json.dump(leaderboards, f)
+except:
+  print("json dumping didn't work")
 
-# generate .json files
-# today's challenges
-with open("dr2-data/challenges_{}.json".format(today[0]["start"][0:10]), "w") as f:
-  json.dump(today, f)
-# yesterday's results
-with open("dr2-data/leaderboards_{}.json".format(yesterday[0]["start"][0:10]), "w") as f:
-  json.dump(leaderboards, f)
+try:
+  # call the PHP script to insert data into database
+  subprocess.call('php /home2/niemann8/dr2-data/insert.php', shell=True)
+except:
+  print("calling PHP insert script didn't work")
 
-
-# call the PHP script to insert data into database
-subprocess.call('php /home2/niemann8/dr2-data/insert.php', shell=True)
+# try:
+#   # call the racer update script
+#   subprocess.call('php /home2/niemann8/dr2-data/racer_update.php', shell=True)
+# except:
+#   print("calling PHP racer update script didn't work")
